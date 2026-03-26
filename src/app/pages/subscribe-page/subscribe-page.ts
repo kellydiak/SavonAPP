a79aaf14e0f0c359b26c1f16f02930b09a84b5bf
kellydiak <yauth11@gmail.com>
2026-03-26 12:00:15 +0100

    commencement 8.4

--- src/app/pages/subscribe-page/subscribe-page.ts
+++ src/app/pages/subscribe-page/subscribe-page.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-subscribe-page',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './subscribe-page.html',
   styleUrl: './subscribe-page.css',
 })
+
 export class SubscribePage {
+
 
 }
